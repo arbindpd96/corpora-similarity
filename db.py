985e2855725e432dbba8bbe0412033f22c0b312e
Christian Hentschel <chentschel@gmail.com>
2023-12-13 11:50:29 +0400

    feat: features extraction

--- db.py
+++ db.py
@@ -2,12 +2,8 @@ import sqlite3
 
 if sqlite3.sqlite_version_info < (3, 35, 0):
     # hotswap to pysqlite-binary if it's too old
-    import subprocess
     import sys
 
-    subprocess.check_call(
-        [sys.executable, "-m", "pip", "install", "pysqlite3-binary"]
-    )
     __import__("pysqlite3")
     sys.modules["sqlite3"] = sys.modules.pop("pysqlite3")
 
